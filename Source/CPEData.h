7d5c58a7b07778867c8b35b02088ec8fa221eaf6
Tony Wei <tony.wei@vudu.com>
2017-08-30 12:33:05 -0700

    Support Carthage
    Support Carthage (for Vudu).
    - Created a new project to compile source code into a framework
    - Cartfile (Carthage) to fetch SWXMLHash as the dependency

---
+++ Source/CPEData.h
@@ -0,0 +1,19 @@
+//
+//  CPEData.h
+//  CPEData
+//
+//  Created by Tony Wei on 8/30/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CPEData.
+FOUNDATION_EXPORT double CPEDataVersionNumber;
+
+//! Project version string for CPEData.
+FOUNDATION_EXPORT const unsigned char CPEDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPEData/PublicHeader.h>
+
+
